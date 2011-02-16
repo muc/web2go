fb0d801b9c8fdbe5bf9d0df64141527dd5d2f6c7
Marc Hitscherich <marc.hitscherich@erdfisch.de>
2011-02-16 15:10:00 +0100

    toobar variation in vplaene modul

--- app/views/vplaene/VplaenePanel.js
+++ app/views/vplaene/VplaenePanel.js
@@ -40,21 +40,64 @@
             }
         };
 
-        this.titleBar = {
-            xtype: 'toolbar',
-            dock : 'top',
-            title: 'Vorlesungspläne',
-            items: [this.homeBtn, {xtype: 'spacer'}, this.wwwBtn]
+//        this.titleBar = {
+//            xtype: 'toolbar',
+//            dock : 'top',
+//            title: 'Vorlesungspläne',
+//            items: [this.homeBtn, {xtype: 'spacer'}, this.wwwBtn]
+//        };
+//
+//        this.toolBar = {
+//            xtype: 'toolbar',
+//            ui: 'dark',
+//            dock: 'top',
+//            items: [this.backBtn]
+//        };
+        
+
+        this.titleBtn = {
+            xtype: 'button',
+            text: 'Vorlesungspläne',
+            iconMask: true,
+            ui: 'plain',
+            cls: 'title-btn',
+            scope: this,
+            handler: function() {
+                if (!this.modulSheet) {
+                    this.modulSheet = new Ext.ActionSheet({
+                        hideOnMaskTap: true,
+                        enter: 'left',
+                        items: [{
+                            text: 'Vorlesungspläne',
+                            ui: 'confirm-round'
+                        }, {
+                            text: 'Mensapläne'
+                        }, {
+                            text: 'Who-is-who'
+                        }, {
+                            text: 'Zimmerbörse'
+                        }, {
+                            text: 'Firmenliste'
+                        }, {
+                            text: 'Dokumente'
+                        }, {
+                            text: 'Studiengänge'
+                        }]
+                    });
+                }
+                this.modulSheet.show();
+            }
         };
         
         this.toolBar = {
             xtype: 'toolbar',
-            ui: 'dark',
             dock: 'top',
-            items: [this.backBtn]
+            ui: 'dark',
+//            title: 'Vorlesungspläne',
+            items: [this.backBtn, {xtype: 'spacer'}, this.titleBtn, {xtype: 'spacer'}, this.homeBtn]
         };
 
-        this.dockedItems = [this.titleBar, this.toolBar];
+        this.dockedItems = [this.toolBar];
 
         web2go.views.VplaenePanel.superclass.initComponent.apply(this, arguments);
     },
