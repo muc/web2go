afbc09c4cda5a7bc429b5805bae18f0387bff4fb
gooney <mail@mweise.de>
2011-02-27 13:16:18 +0100

    erste richtige DHBW-Icons fuer Homescreen integriert

--- app/views/HomePanel.js
+++ app/views/HomePanel.js
@@ -5,13 +5,13 @@
  */
 
 web2go.Modules = [
-  {name: 'Vorlesungspläne', controller: 'web2go.controllers.vplaene', action: 'index', icon: 'icon4.png'},
-  {name: 'Mensa', controller: 'web2go.controllers.mensa', action: 'index', icon: 'icon7.jpg'},
-  {name: 'Who-is-Who', controller: 'web2go.controllers.whoiswho', action: 'index', icon: 'icon1.jpg'},
-  {name: 'Zimmerbörse', controller: 'web2go.controllers.zimmerboerse', action: 'index', icon: 'icon6.jpg'},
-  {name: 'Firmenliste', controller: 'web2go.controllers.firmenliste', action: 'index', icon: 'icon3.jpg'},
-  {name: 'Dokumente', controller: 'web2go.controllers.dokumente', action: 'index', icon: 'icon5.jpg'},
-  {name: 'Studiengänge', controller: 'web2go.controllers.studiengaenge', action: 'index', icon: 'icon2.png'},
+  {name: 'Vorlesungspläne', controller: 'web2go.controllers.vplaene', action: 'index', icon: 'icon_vorlesungsplaene.png'},
+  {name: 'Mensa', controller: 'web2go.controllers.mensa', action: 'index', icon: 'icon_mensaplaene.png'},
+  {name: 'Who-is-Who', controller: 'web2go.controllers.whoiswho', action: 'index', icon: 'icon_who-is-who.png'},
+  {name: 'Zimmerbörse', controller: 'web2go.controllers.zimmerboerse', action: 'index', icon: 'icon_zimmerboerse.png'},
+  {name: 'Firmenliste', controller: 'web2go.controllers.firmenliste', action: 'index', icon: 'icon_firmenliste.png'},
+  {name: 'Dokumente', controller: 'web2go.controllers.dokumente', action: 'index', icon: 'icon_dokumente.png'},
+  {name: 'Studiengänge', controller: 'web2go.controllers.studiengaenge', action: 'index', icon: 'icon_studiengaenge.png'},
 ];
 
 web2go.views.HomePanel = Ext.extend(Ext.Panel, {
