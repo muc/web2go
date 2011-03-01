a26ad44ee56a5ec46a7ed8c83a99f33eba0cab85
muc <git@damuc.de>
2011-03-01 20:49:25 +0100

    pdf redirect for vplaene

--- app/views/vplaene/VplaeneZeit.js
+++ app/views/vplaene/VplaeneZeit.js
@@ -5,40 +5,61 @@
  */
 
 web2go.views.VplaeneZeit = Ext.extend(Ext.form.FormPanel, {
-  submitOnAction: false,
+    submitOnAction: false,
 
-  initComponent: function() {
-    this.items = [{
-        xtype: 'fieldset',
-        title: 'Zeitraum',
-        items: [{
-          xtype: 'selectfield',
-          name: 'timeframe',
-          valueField: 'kw',
-          displayField: 'time',
-          store : web2go.stores.vplaeneTimes
-        }]
-    }];
-    web2go.stores.vplaeneTimes.load({
-      scope   : this,
-      callback: function() {
-        var r = web2go.stores.vplaeneTimes.findRecord('curr', true);
-        console.log(this.items);
-        this.setValues({
-          'timeframe': r.get('time')
+    initComponent: function() {
+        this.timeSelectField = new Ext.form.Select({
+            name: 'timeframe',
+            valueField: 'kw',
+            displayField: 'time',
+            store : web2go.stores.vplaeneTimes
         });
-      }
-    });
-    
-    web2go.views.VplaeneZeit.superclass.initComponent.apply(this, arguments);
-  },
+        
+        this.bottomBar = new Ext.Toolbar({
+            dock: 'bottom',
+            ui: 'dark',
+            items: [{
+                xtype: 'spacer'
+            }, {
+                text: 'Anzeigen',
+                ui: 'action',
+                scope: this,
+                handler: this.showPDF
+            }]
+        });
 
-  getCourse: function() {
-    return this.course;
-  },
+        this.dockedItems = [this.bottomBar];
+        
+        this.items = [{
+            xtype: 'fieldset',
+            title: 'Zeitraum',
+            items: [this.timeSelectField]
+        }];
+        web2go.stores.vplaeneTimes.load({
+            scope   : this,
+            callback: function() {
+                var index = web2go.stores.vplaeneTimes.find('curr', true);
+                this.setValues({
+                    'timeframe': web2go.stores.vplaeneTimes.findRecord('curr', true).get('kw')
+                });
+            }
+        });
+    
+        web2go.views.VplaeneZeit.superclass.initComponent.apply(this, arguments);
+    },
+
+    getCourse: function() {
+        return this.course;
+    },
   
-  setCourse: function(val) {
-    this.course = val;
-  }
+    setCourse: function(val) {
+        this.course = val;
+    },
+    
+    showPDF: function() {
+        var url = "http://pollux.dhbw-mosbach.de/cmos_extern_kurs_ext/",
+            kw  = this.timeSelectField.getValue()
+        window.location.href=url + this.course.toLowerCase() + '-' + kw + '.pdf';
+    }
 
 });
--- sample_data/vplaene_times.php
+++ sample_data/vplaene_times.php
@@ -1,11 +1,28 @@
 <?php
 
+$times = array();
+
+$firstJan = mktime(0, 0, 0, 1, 1, date('Y'));
+$firstMonInJan = date('N', $firstJan) == 1 ? $firstJan : strtotime('next Monday', $firstJan);
+$start = strtotime('-16 weeks', $firstMonInJan);
+
+$inThisYear = true;
+$next = $start;
+while ($inThisYear) {
+    $time_start = $next;
+    $time_end = strtotime('next Friday', strtotime('+3 weeks', $time_start));
+    
+    $times[] = array(
+        'time' => date('d.m.Y', $time_start) . '-' . date('d.m.Y', $time_end),
+        'kw' => date('Y', $time_start) . '-' . date('W', $time_start),
+        'curr' => time() >= $time_start && time() <= $time_end
+    );
+    $next = strtotime('next Monday', $time_end);
+    $inThisYear = date('Y', $next) <= date(Y);
+}
+    
 echo json_encode(array(
-    'data' => array(
-        array('time' => '04.01.2011-27.02.2011', 'kw' => '2011-05', 'curr' => false),
-        array('time' => '28.02.2011-25.03.2011', 'kw' => '2011-09', 'curr' => true),
-        array('time' => '25.03.2011-22.04.2011', 'kw' => '2011-13', 'curr' => false)
-    )
+    'data' => $times
 ));
 
 
